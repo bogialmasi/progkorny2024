302017bf1861d553d1fd567d7d40bbe0ca1e35a8
bogialmasi <boglarkaalmasi@gmail.com>
2024-05-15 18:47:03 +0200

    cleanup import

--- src/test/java/org/nye/progkorny/service/EventServiceTest.java
+++ src/test/java/org/nye/progkorny/service/EventServiceTest.java
@@ -2,15 +2,11 @@ package org.nye.progkorny.service;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.nye.progkorny.controller.EventController;
 import org.nye.progkorny.model.Event;
-import org.nye.progkorny.model.EventType;
 import org.nye.progkorny.repository.impl.EventRepository;
 import org.nye.progkorny.service.impl.EventService;
-import org.nye.progkorny.service.impl.EventTypeService;
 
 import java.sql.SQLException;
 import java.sql.Timestamp;
@@ -21,7 +17,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class EventServiceTest {
